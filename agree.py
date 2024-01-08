990f551633be266c9e827e9489ec17907bca6725
sserwangaedrisa <sserwangaedrisa@gmail.com>
2024-01-08 15:20:55 +0300

    Updated the code to use a loop through the list of possiblilities

--- agree.py
+++ agree.py
@@ -1,7 +1,7 @@
 s = input("Do you agree: ")
-if s =='y' or s == "Y":
+if s in ['y', 'Y']:
     print('Yes you have agreed')
-elif s== "n" or s == "N": 
+elif s in ['n', 'N']:
     print("No you don't agree")
 else:
     print("You haven't decided")
