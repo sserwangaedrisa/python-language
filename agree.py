c600527b0da177b1bedacd68aea31b083f401450
sserwangaedrisa <sserwangaedrisa@gmail.com>
2024-01-08 15:16:37 +0300

    Updated the agree.py file

--- agree.py
+++ agree.py
@@ -1,5 +1,7 @@
-s = int(input("Do you agree: "))
+s = input("Do you agree: ")
 if s =='y' or s == "Y":
     print('Yes you have agreed')
 elif s== "n" or s == "N": 
     print("No you don't agree")
+else:
+    print("You haven't decided")
