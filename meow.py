d8dadc6dbe2244bb52de648126e897f245aae461
sserwangaedrisa <sserwangaedrisa@gmail.com>
2024-01-08 15:53:07 +0300

    Updated the file

--- meow.py
+++ meow.py
@@ -1,6 +1,7 @@
 # printing meow for the n times ' n is the number of times the user will input'
 def main():
-    for i in range(3):
+    n = int(input('How many times i should meow: '))
+    for i in range(n):
         meow()
 
 
