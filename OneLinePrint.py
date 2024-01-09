085a7759630ad019f8d962428d92b5ccb48a6085
sserwangaedrisa <sserwangaedrisa@gmail.com>
2024-01-09 23:40:32 +0300

    Updated the file

--- OneLinePrint.py
+++ OneLinePrint.py
@@ -1,8 +1,10 @@
-x = int(input('Enter the number of hashes'))
+x = int(input('Enter the number of hashes: '))
 for i in range(x):
     for j in range(x):
         print('#',end='')
     print()
+
+print("The second attempt is here")
 
 for k in range(x):
     print('#'*x)
