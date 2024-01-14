639856554d6e6907f3b52771d7d2fc99d4824384
sserwangaedrisa <sserwangaedrisa@gmail.com>
2024-01-15 00:09:19 +0300

    Added a note in the work

--- great.py
+++ great.py
@@ -5,5 +5,6 @@ for i in argv:
     print(f"{i}")
 print()
 # In case you want to print a portion of the list
+# If you want to access the whole of the argv list, you can use the code  " argv[0:]"
 for arg in argv[1:2]:
     print(f"{arg}")
