2c1979c4f0fd3cae07ef1c2ab22b8d8b1f949d09
sserwangaedrisa <sserwangaedrisa@gmail.com>
2024-01-15 00:42:21 +0300

    Updated the code

--- names.py
+++ names.py
@@ -1,10 +1,19 @@
 import sys
 names = ['Bill', 'Charlie', "Fred", 'George', "Percy", 'Ron']
 name = input("What is the name you want: ")
-for n in names:
-    if name == n:
-        print(f"found {n}")
-        sys.exit(0)
+# for n in names:
+#     if name == n:
+#         print(f"found {n}")
+#         sys.exit(0)
 
-print("Not found")
-sys.exit(1)
+# print("Not found")
+# sys.exit(1)
+# The above code is the older way of doing it in c
+# The newer version is here
+
+if name in names:
+    print('Found')
+    sys.exit(1)
+
+print('not foud')
+sys.exit(0)
