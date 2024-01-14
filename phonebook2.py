da855bb0baab59ae116c6bdc32446a3aae0efbbf
sserwangaedrisa <sserwangaedrisa@gmail.com>
2024-01-15 01:37:49 +0300

    updated the file

--- phonebook2.py
+++ phonebook2.py
@@ -1,2 +1,15 @@
 import csv
 file = open('phonebook.csv', 'a')
+Name = input('Name: ')
+Number = input('Number: ')
+
+writer = csv.writer(file)
+writer.writerow([Name, Number])
+file.close()
+# alternatively, we can use the below code in order to eliminate the error that can rise from forgettin to close the open file. This code will automatically close the file after using it.
+# with open('phonebook.csv', "a") as file:
+#     Name = input('Name: ')
+#     Number = input('Number: ')
+
+#     writer = csv.writer(file)
+#     writer.writerow([Name, Number])
