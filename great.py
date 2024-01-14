8fe06374812cb1af65716fe3b01c3bfab60e7bdc
sserwangaedrisa <sserwangaedrisa@gmail.com>
2024-01-14 23:58:29 +0300

    Made the code that allows the access of command line arguments

---
+++ great.py
@@ -0,0 +1,5 @@
+# In this code , we are importing the built library of python called 'sys' (system), this helps us access alot of system info including the command line arguments
+from sys import argv
+
+for i in argv:
+    print(f"{i}")
